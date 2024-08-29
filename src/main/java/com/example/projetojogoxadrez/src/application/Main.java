693bdb9201b0ab67f13dd0b0c98827d4c81ed466
Douglas Bastos <dautodouglasdauto@gmail.com>
2024-08-29 19:06:11 -0300

    Primeira versão

---
+++ src/main/java/com/example/projetojogoxadrez/src/application/Main.java
@@ -0,0 +1,9 @@
+package com.example.projetojogoxadrez.src.application;
+
+import com.example.projetojogoxadrez.src.boardgame.Board;
+
+public class Main {
+    public static void main(String[] args) {
+        Board board = new Board(2, 4);
+    }
+}
---
+++ src/main/java/com/example/projetojogoxadrez/src/boardgame/Position.java
@@ -0,0 +1,32 @@
+package com.example.projetojogoxadrez.src.boardgame;
+
+public class Position {
+    private Integer row;
+    private  Integer column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+}
